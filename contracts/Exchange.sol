c3f84002de3a2daed09ab7f1553db60eabccb455
cmdhema <cmdhema@gmail.com>
2022-08-10 13:17:43 +0900

    implement start addLiquidity

--- contracts/Exchange.sol
+++ contracts/Exchange.sol
@@ -4,6 +4,13 @@ pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract Exchange {
+    IERC20 token;
 
+    constructor (address _token) {
+        token = IERC20(_token);
+    }
+    
+    function addLiquidity(uint256 _tokenAmount) public payable {
+        token.transferFrom(msg.sender, address(this), _tokenAmount);
+    }
 }
-
--- test/Exchange.ts
+++ test/Exchange.ts
@@ -27,6 +27,12 @@ describe("Exchange", () => {
     });
 
     describe("addLiquidity", async () => {
-        
-    });
+        it("adds liquidity", async () => {
+          await token.approve(exchange.address, toWei(200));
+          await exchange.addLiquidity(toWei(200), { value: toWei(100) });
+    
+          expect(await getBalance(exchange.address)).to.equal(toWei(100));
+          expect(await token.balanceOf(exchange.address)).to.equal(toWei(200));
+        });
+      });
 })
